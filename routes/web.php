994c9be9e6a3462ac6e0552398436ff52c5f4b52
pwkodok77 <sandipw7777@gmail.com>
2024-02-15 22:45:28 +0700

    crud users

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('pages.app.dashboard-siakad',['type_menu' => '']);
+// });
+
 Route::get('/', function () {
-    return view('pages.app.dashboard-siakad',['type_menu' => '']);
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
+Route::middleware(['auth'])->group(function () {
+    Route::get('home',function(){
+        return view('pages.app.dashboard-siakad',['type_menu'=>'']);
+    })->name('home');
+    Route::resource('user', UserController::class);
+    // supaya yg bisa proses yg tambah user dll orang yg sudah login
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})->name('register');
+});
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('forgot');
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('forgot');
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('reset-password');
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset-password');
